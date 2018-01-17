d750d2a48750692ad3cfd24f3df5ff99ed594dce
Raman <ramandeep1794@gmail.com>
2018-01-17 18:36:37 +0530

    name rectification

---
+++ src/components/cardDetailComponent.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Container from 'muicss/lib/react/container';
+import Card from 'material-ui/Card';
+import Row from 'muicss/lib/react/row';
+import Col from 'muicss/lib/react/col';
+
+
+class CardDetailComponent extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <Container fluid={true}>
+                <div>
+                    
+                </div>
+            </Container>
+        );
+    }
+}
+export default CardDetailComponent;
