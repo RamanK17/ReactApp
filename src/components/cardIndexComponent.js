a7609fd9b465748258568d4e9341ba0778b25fd0
Raman <ramandeep1794@gmail.com>
2018-01-17 15:13:12 +0530

    data is passed as props to cardDetailComponent

--- src/components/cardIndexComponent.js
+++ src/components/cardIndexComponent.js
@@ -5,6 +5,7 @@ import Card from 'material-ui/Card';
 import Row from 'muicss/lib/react/row';
 import Col from 'muicss/lib/react/col';
 
+import CardDetailComponent from "./cardDetailComponent.js";//cards having data with like/dislike and bookmark actions
 
 class CardsIndexComponent extends React.Component{
     constructor(props){
@@ -19,7 +20,7 @@ class CardsIndexComponent extends React.Component{
                         <Col sm="1"></Col>
                         <Col sm="10">
                             <Card className="cards">
-                                
+                                <CardDetailComponent detail={info} index={index} />
                             </Card>
                         </Col>
                         <Col sm="1"></Col>
