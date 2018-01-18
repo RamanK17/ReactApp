8b6fd80b586ab8c1cdaff4267e306ffdd5e06d0a
Raman <ramandeep1794@gmail.com>
2018-01-18 20:03:01 +0530

    routes included in main.js

--- routes.js
+++
@@ -1,9 +0,0 @@
-
-import CardsIndexComponent from "./src/components/cardIndexComponent";
-import CardDetailComponent from "./src/components/CardDetailComponent";
-
-export const routes = [
-        {path:"/", component:CardsIndexComponent},
-        {path:"/:details?" ,component:CardDetailComponent}
-];
-
