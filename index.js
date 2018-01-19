9160d7b822cf1fe87bd5bb3f675e3806238e1884
Raman <ramandeep1794@gmail.com>
2018-01-19 19:24:42 +0530

    redux required

--- index.js
+++ index.js
@@ -3,14 +3,24 @@ import {render} from 'react-dom';
 import ReactDOM from 'react-dom';
 import { BrowserRouter} from 'react-router-dom';
 
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import promise from 'redux-promise-middleware';
+import {createLogger} from 'redux-logger';
+
 import './src/css/mui.min.css';
 import './src/css/style.scss';
 import App from './src/components/app.js';
+import allReducers from './src/reducers';
+
+const store = createStore(allReducers,applyMiddleware(thunk, promise(), createLogger()));
 
 render(
-
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>,
+    <Provider  store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('app')
 );
